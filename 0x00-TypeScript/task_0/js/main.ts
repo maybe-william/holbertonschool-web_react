a9ca3f2d6a8bbc87a54871c2d1c21aa569bd3ac4
william <191marquis@gmail.com>
2020-10-27 16:25:33 -0400

    [DEV] Do first try

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,25 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string,
+}
+
+const will: Student = {
+    firstName: "Will",
+    lastName: "Noneyo",
+    age: 18,
+    location: "N/A",
+}
+
+const not_will: Student = {
+    firstName: "Sue",
+    lastName: "Jones",
+    age: 23,
+    location: "NY",
+}
+
+const studentsList = [will, not_will]
+
+
+
