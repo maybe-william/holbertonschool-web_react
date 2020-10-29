9289b8310a052dbd87efac1c1e3b5607caa56c90
william <191marquis@gmail.com>
2020-10-29 16:01:45 -0400

    [BUGFIX] Try a couple other things

--- 0x00-TypeScript/task_3/js/crud.d.ts
+++ 0x00-TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,6 @@
 /* comment */
+import { RowID, RowElement } from './interface';
+
+export function insertRow(row: RowElement): number;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,3 +1,4 @@
 /* /// <reference path = ./crud.d.ts /> import * as CRUD from ./interface ./crud.js */
 /* const row: RowElement = firstName: 'Guilaume' lastName: 'Salva' */
+/* const row: RowElement = firstName: 'Guillaume' lastName: 'Salva' */
 /* CRUD.updateRow(newRowID, updatedRow) CRUD.deleteRow(newRowID) */
