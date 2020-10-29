2bc43fa38e66bc6c800a41f4807463a5830d6a3a
william <191marquis@gmail.com>
2020-10-29 15:46:17 -0400

    [INIT] Do first try

---
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -0,0 +1,3 @@
+/* /// <reference path = ./crud.d.ts /> import * as CRUD from ./interface ./crud.js */
+/* const row: RowElement = firstName: 'Guilaume' lastName: 'Salva' */
+/* CRUD.updateRow(newRowID, updatedRow) CRUD.deleteRow(newRowID) */
