7405c4147327b7da8ff83b8685f6630ae3374a0f
william <191marquis@gmail.com>
2020-11-03 15:41:53 -0500

    [BUGFIX] Add plugins

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   mode: "development",
   output: {
     path:path.resolve(__dirname, "public"),
