efd54b322f8f6266c970aa7d01d563bfe12c341a
william <191marquis@gmail.com>
2020-11-03 15:25:59 -0500

    [BUGFIX] Set mode to development

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: "./js/dashboard_main",
-  mode: "production",
+  mode: "development",
   output: {
     path:path.resolve(__dirname, "public"),
     filename: "bundle.js"
