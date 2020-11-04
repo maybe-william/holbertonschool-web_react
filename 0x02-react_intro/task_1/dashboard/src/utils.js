4e31fb1d09eb466a776c5fd08558ae1dbe9f51ae
william <191marquis@gmail.com>
2020-11-04 18:54:08 -0500

    [DEV] Do task 1

---
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,10 @@
+export function getFullYear() {
+  return "2020";
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return "Holberton School";
+  }
+  return "Holberton School main dashboard";
+}
