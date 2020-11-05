67892c3a121509dda360763ebdcdbf745a7a5c23
william <191marquis@gmail.com>
2020-11-04 20:20:17 -0500

    [DEV] Add basic utils test

---
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import logo from './holberton-logo.jpg';
+import {getFullYear, getFooterCopy} from './utils.js';
+import './App.css';
+import {Notifications} from './Notifications.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="root-notifications">
+        {Notifications()}
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          School dashboard
+        </h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email: </label>
+          <input id="email" type="text"/>
+          <label htmlFor="password"> Password: </label>
+          <input id="password" type="text"/>
+          <button>ok</button>
+        </form>
+      </main>
+      <footer className="App-footer">
+        <p className="copyright-text">Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
