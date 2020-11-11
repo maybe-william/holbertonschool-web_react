d3eb8e66a4dbc2f2269d4c36d72cc7f0bb42dd78
william <191marquis@gmail.com>
2020-11-10 21:27:42 -0500

    [DEV] Do task 0

--- 0x03-react_props/task_0/dashboard/src/App/App.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import logo from '../assets/holberton-logo.jpg';
-import {getFullYear, getFooterCopy} from '../utils/utils.js';
 import './App.css';
 import Notifications from '../Notifications/Notifications.js';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import Footer from '../Footer/Footer.js';
 
 function App() {
   return (
+    <React.Fragment>
+    {Notifications()}
     <div className="App">
-      <div id="root-notifications">
-        {Notifications()}
+      {Header()}
+      <div className="App-body">
+        {Login()}
       </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          School dashboard
-        </h1>
-      </header>
-      <main className="App-body">
-        <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email: </label>
-          <input id="email" type="text"/>
-          <label htmlFor="password"> Password: </label>
-          <input id="password" type="text"/>
-          <button>ok</button>
-        </form>
-      </main>
-      <footer className="App-footer">
-        <p className="copyright-text">Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </footer>
+      <div className="App-footer">
+        {Footer()}
+      </div>
     </div>
+    </React.Fragment>
   );
 }
 
