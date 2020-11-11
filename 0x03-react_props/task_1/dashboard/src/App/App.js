73114ef796bd7f35f420273bb6085ce1e2b16f98
william <191marquis@gmail.com>
2020-11-10 21:42:38 -0500

    [INIT] Create task 1 files

---
+++ 0x03-react_props/task_1/dashboard/src/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications.js';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import Footer from '../Footer/Footer.js';
+
+function App() {
+  return (
+    <React.Fragment>
+    {Notifications()}
+    <div className="App">
+      {Header()}
+      <div className="App-body">
+        {Login()}
+      </div>
+      <div className="App-footer">
+        {Footer()}
+      </div>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
