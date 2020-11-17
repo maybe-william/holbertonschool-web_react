96588c35c05e93dfef85b1ba3b4e68902b5ac4c8
william <191marquis@gmail.com>
2020-11-16 22:50:52 -0500

    [DEV] Do task 1

--- 0x04-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x04-React_component/task_1/dashboard/src/App/App.test.js
@@ -7,7 +7,10 @@ import Header from '../Header/Header';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import Footer from '../Footer/Footer';
+import chai from 'chai';
+import chaiJestMock from 'chai-jest-mocks';
 
+chai.use(chaiJestMock);
 
 
 describe('<App />', () => {
@@ -39,5 +42,19 @@ describe('<App />', () => {
   it('does render a CourseList component if logged in', () => {
     const wrapper = shallow(<App isLoggedIn={true} />);
     expect(wrapper.find(CourseList)).to.have.lengthOf(1);
+  });
+});
+
+
+describe('The logOut functionality', () => {
+  it('gets called, and alerts with the correct string', () => {
+    const myLogOut = jest.fn(() => undefined);
+    const myAlert = jest.spyOn(global, 'alert');
+
+    const wrapper = shallow(<App logOut={myLogOut} />)
+
+    expect(myAlert);
+    expect(myLogOut);
+    jest.restoreAllMocks();
   });
 });
