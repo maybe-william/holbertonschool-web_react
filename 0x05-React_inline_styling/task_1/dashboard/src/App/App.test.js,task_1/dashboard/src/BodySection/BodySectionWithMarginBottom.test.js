f2a257df90596a879cd32d0e9654cc74549813ae
william <191marquis@gmail.com>
2020-11-18 21:06:52 -0500

    [BUGFIX] Add in misnamed files

---
+++ 0x05-React_inline_styling/task_1/dashboard/src/App/App.test.js,task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+describe('This test', () => {
+  it('passes', () => {
+    expect(true).to.equal(true);
+  });
+});
