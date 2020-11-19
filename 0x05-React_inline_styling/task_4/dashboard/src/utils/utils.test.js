24f8ece17f72acda7d58cf986fce80100f579e56
william <191marquis@gmail.com>
2020-11-18 22:48:38 -0500

    [DEV] Do task 4

---
+++ 0x05-React_inline_styling/task_4/dashboard/src/utils/utils.test.js
@@ -0,0 +1,14 @@
+const { getFullYear, getFooterCopy, getLatestNotification } = require('./utils');
+
+test('Year returns 2020', () => {
+  expect(getFullYear()).toBe("2020");
+})
+
+test('getFooterCopy works in both cases', () => {
+  expect(getFooterCopy(true)).toBe("Holberton School");
+  expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+})
+
+test('getLatestNotification works', () => {
+  expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
+})
