d15fa783dcdb29d625a6e5e962b053971f79330a
william <191marquis@gmail.com>
2020-11-23 19:24:38 -0500

    [DEV] Do task 1

---
+++ 0x06-react_state/task_1/dashboard/src/HOC/WithLogging.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Footer from '../Footer/Footer';
+
+describe('This test', () => {
+  it('passes', () => {
+    expect(true).to.equal(true);
+  });
+});
---
+++ 0x06-react_state/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {StyleSheet, css} from 'aphrodite';
+
+const styles = StyleSheet.create({
+    formItem: {
+        marginRight: 10
+    },
+    formSection: {
+        display: "flex",
+        flexDirection: "row",
+        '@media (max-width: 700px)': {
+            display: "flex",
+            flexDirection: "column"
+        }
+    }
+});
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+            email: "",
+            password: "",
+            enableSubmit: false,
+        };
+
+        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+    }
+
+    handleLoginSubmit(e) {
+        e.preventDefault();
+        this.setState({
+            isLoggedIn: true
+        });
+    }
+
+    handleChangeEmail(e) {
+        this.setState({
+            email: e.target.value,
+        });
+        this.setState((state, props) => ({
+            enableSubmit: (state.email !== "" && state.password !== ""),
+        }));
+    }
+    handleChangePassword(e) {
+        this.setState({
+            password: e.target.value,
+        });
+        this.setState((state, props) => ({
+            enableSubmit: (state.email !== "" && state.password !== "")
+        }));
+    }
+
+    render() {
+
+
+        return (
+            <React.Fragment>
+            <p>Login to access the full dashboard</p>
+            <form onSubmit={this.handleLoginSubmit} className={css(styles.formSection)}>
+            <div>
+            <label htmlFor="email" className={css(styles.formItem)}>Email: </label>
+            <input id="email" type="text" value={this.state.email} onChange={this.handleChangeEmail} className={css(styles.formItem)}/>
+            </div>
+            <div>
+            <label htmlFor="password" className={css(styles.formItem)}> Password: </label>
+            <input id="password" type="password" value={this.state.password} onChange={this.handleChangePassword} className={css(styles.formItem)}/>
+            </div>
+            <div>
+            <input type="submit" value="ok" disabled={!this.state.enableSubmit} className={css(styles.formItem)} />
+            </div>
+            </form>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Login;
