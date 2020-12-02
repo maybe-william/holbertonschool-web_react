e077fd2e7dc56c0235d060c5eab219d7326d0aa0
william <191marquis@gmail.com>
2020-12-01 19:17:39 -0500

    [BUGFIX] Use merge instead

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -9,4 +9,7 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+export const map2 = map.merge(Map({
+  2: 'Benjamin',
+  4: 'Oliver',
+}));
