4e5fa35548b8fc05c69bf487b1cbdc4c8cf85b89
william <191marquis@gmail.com>
2020-12-01 20:04:05 -0500

    [BUGFIX] Add two List calls

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 
 export function concatElements(page1, page2) {
-  return List(page1).concat(page2);
+  return List(page1).concat(List(page2));
 }
 
 export function mergeElements(page1, page2) {
-  return List(page1).merge(page2);
+  return List(page1).merge(List(page2));
 }
