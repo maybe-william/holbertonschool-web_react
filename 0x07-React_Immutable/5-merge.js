975eaf8806cf5d6b744c4e9cec481a998c887678
william <191marquis@gmail.com>
2020-12-01 19:48:34 -0500

    [DEV] Do first try

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,9 @@
+import { List } from 'immutable';
+
+export function concatElements(page1, page2) {
+  return List(page1).concat(page2);
+}
+
+export function mergeElements(page1, page2) {
+  return List(page1).merge(page2);
+}
