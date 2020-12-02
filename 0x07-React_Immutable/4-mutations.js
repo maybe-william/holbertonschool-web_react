47b1adace24df65c6a9ab8170a61781d35c17962
william <191marquis@gmail.com>
2020-12-01 19:42:50 -0500

    [BUGFIX] Change map to map1

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-const map = Map({
+const map1 = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,6 +9,6 @@ const map = Map({
   6: 'Lucas',
 });
 
-const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+const map2 = map1.set(2, 'Benjamin').set(4, 'Oliver');
 
-export { map, map2 };
+export { map1, map2 };
