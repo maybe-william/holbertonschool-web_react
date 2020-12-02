1d2c989b95f2044a092213647b98d1b16a8e14d0
william <191marquis@gmail.com>
2020-12-01 19:20:20 -0500

    [BUGFIX] Use fromJS instead

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
-import { getIn, Map } from 'immutable';
+import { getIn, fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  return getIn(Map(object), array, undefined);
+  return getIn(fromJS(object), array, undefined);
 }
