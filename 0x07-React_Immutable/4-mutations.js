f81815d3ac7b3cc2793e870b2740ab765bcc0358
william <191marquis@gmail.com>
2020-12-01 19:36:26 -0500

    [BUGFIX] Change form of object?

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,11 +1,14 @@
 import { Map } from 'immutable';
 
-export const map = Map()
-  .set(1, 'Liam')
-  .set(2, 'Noah')
-  .set(3, 'Elijah')
-  .set(4, 'Oliver')
-  .set(5, 'Jacob')
-  .set(6, 'Lucas');
+export const map = Map(
+{
+     1: 'Liam',
+     2: 'Noah',
+     3: 'Elijah',
+     4: 'Oliver',
+     5: 'Jacob',
+     6: 'Lucas',
+}
+);
 
 export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
